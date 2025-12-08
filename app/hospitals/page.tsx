fe5a4894c3d16931b3477bac2ab2de9ac127c564
KooruMahey <koorumahey01@gmail.com>
2025-12-08 20:01:31 +0530

    feat: initialize backend and CMS modules with basic structure and configuration files
    - Added models, controllers, services, and routes for consent, cost-calculator, notifications, payment, profile, search, telemedicine, visa, and wellness modules.
    - Created necessary templates for notifications and visa services.
    - Established foundational scripts for database management and deployment.
    - Included Docker and Kubernetes configurations for backend and CMS.
    - Introduced monitoring configurations for logs and Prometheus.
    - Set up localization files for multiple languages.
    - Added security policies and compliance documentation.

---
+++ app/hospitals/page.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+
+export default function HospitalsPage() {
+    return( 
+        <>
+    <div>Hospitals Page</div>
+    <div className='min-h-screen bg-linear-120 to-25% bg-white'></div>
+    </>
+    )
+}
